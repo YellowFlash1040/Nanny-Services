7696b86466b59c2b6d6e3020a3939ba1a41500bd
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-21 11:59:37 +0200

    I added AppointmentForm component

---
+++ src/components/AppointmentForm/AppointmentForm.tsx
@@ -0,0 +1,92 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { Button, TimePicker } from '../../components';
+
+import schema from './shema';
+
+import s from './AppointmentForm.module.css';
+import { Controller, useForm } from 'react-hook-form';
+
+// interface AppointmentFormProps {}
+
+interface FormFields {
+  address: string;
+  phone: string;
+  age: number;
+  time?: string | undefined;
+  email: string;
+  fullname: string;
+  comment?: string | undefined;
+}
+
+const AppointmentForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    control
+  } = useForm({
+    defaultValues: { time: '10:00' },
+    resolver: yupResolver(schema)
+  });
+
+  const handleFormSubmit = (data: FormFields) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <div className={s.twoFieldsContainer}>
+        <label className={s.formField}>
+          <input type="text" placeholder="Address" {...register('address')} />
+          {errors.address && (
+            <span className={s.errorSpan}>{errors.address.message}</span>
+          )}
+        </label>
+        <label className={s.formField}>
+          <input type="tel" placeholder="+380" {...register('phone')} />
+          {errors.phone && <span className={s.errorSpan}>{errors.phone.message}</span>}
+        </label>
+      </div>
+      <div className={s.twoFieldsContainer}>
+        <label className={s.formField}>
+          <input type="text" placeholder="Child's age" {...register('age')} />
+          {errors.age && <span className={s.errorSpan}>{errors.age.message}</span>}
+        </label>
+        <Controller
+          name="time"
+          control={control}
+          render={({ field }) => (
+            <TimePicker
+              value={field.value!}
+              onChange={field.onChange}
+              dropDownClassName={s.timePicker}
+            />
+          )}
+        />
+      </div>
+      <label className={s.formField}>
+        <input type="email" placeholder="Email" {...register('email')} />
+        {errors.email && <span className={s.errorSpan}>{errors.email.message}</span>}
+      </label>
+      <label className={s.formField}>
+        <input
+          type="text"
+          placeholder="Father's or mother's name"
+          {...register('fullname')}
+        />
+        {errors.fullname && (
+          <span className={s.errorSpan}>{errors.fullname.message}</span>
+        )}
+      </label>
+      <textarea placeholder="Comment" {...register('comment')} />
+      <Button className={s.sendButton} type="submit" onClick={() => {}}>
+        Send
+      </Button>
+    </form>
+  );
+};
+
+export default AppointmentForm;
---
+++ src/components/AppointmentForm/shema.ts
@@ -0,0 +1,21 @@
+import * as yup from 'yup';
+
+const schema = yup.object({
+  address: yup.string().required(),
+  phone: yup.string().required(),
+  age: yup
+    .number()
+    .transform((value, originalValue) => {
+      return originalValue === '' ? undefined : value;
+    })
+    .typeError('The value must be a number')
+    .min(0, 'x > 0 and x <= 12')
+    .max(12, 'x > 0 and x <= 12')
+    .required(),
+  time: yup.string() /* .required() */,
+  email: yup.string().email().required(),
+  fullname: yup.string().min(3).max(48).required(),
+  comment: yup.string().max(500)
+});
+
+export default schema;

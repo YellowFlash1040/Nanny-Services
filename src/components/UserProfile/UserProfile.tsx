9df3760ae3e14af5865864bd895658259c2ee831
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-21 21:46:13 +0200

    Update UserProfile component
    I modified this component. Now it’s also a dropdown with which you can change the application theme. For more details take a look at the code in the changes log

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -1,6 +1,9 @@
 import clsx from 'clsx';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 
 import { UserProfileIcon } from '../../assets';
+import { Theme } from '../../types';
+import { changeAppTheme } from '../../helpers';
 
 import s from './UserProfile.module.css';
 
@@ -9,13 +12,63 @@ interface UserProfileProps {
 }
 
 const UserProfile = ({ className }: UserProfileProps) => {
+  const optionsList = useRef(['Red', 'Blue', 'Green']);
+  const popupRef = useRef<HTMLDivElement>(null);
+  const [isOpened, setIsOpened] = useState(false);
+  const [theme, setTheme] = useState<Theme>(Theme.Red);
+
+  const handleItemSelect = (event: MouseEvent) => {
+    setTheme((event.target as HTMLButtonElement).textContent as Theme);
+  };
+
+  useEffect(() => {
+    const handleClickOutside: EventListener = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+        setIsOpened(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    changeAppTheme(theme);
+  }, [theme]);
+
   return (
-    <button className={clsx(s.userProfileButton, className)}>
-      <div className={s.avatarContainer}>
-        <UserProfileIcon width={24} height={24} />
-      </div>
-      <p>Ilona</p>
-    </button>
+    <div className={s.dropDown} ref={popupRef}>
+      <button
+        className={clsx(s.userProfileButton, className)}
+        type="button"
+        onClick={() => setIsOpened(!isOpened)}
+      >
+        <div className={s.avatarContainer}>
+          <UserProfileIcon width={24} height={24} />
+        </div>
+        <p>Ilona</p>
+      </button>
+      <ul
+        className={clsx(s.optionsList, { [s.optionsListOpened]: isOpened })}
+        onClick={handleItemSelect}
+      >
+        {optionsList.current.map((option) => (
+          <li key={option}>
+            <button
+              className={clsx(s.optionButton, { [s.selectedItem]: theme === option })}
+              type="button"
+              tabIndex={!isOpened ? -1 : undefined}
+              onClick={() => setIsOpened(false)}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 

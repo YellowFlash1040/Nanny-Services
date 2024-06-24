68316d91e16ffd083b4257d9629c115b7a89f9b1
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-24 23:42:25 +0200

    Update for Header.tsx
    Now when a user is loggedIn the UserPanel is being displayed and when he is not the Login and Register buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,29 @@
-import { PageContainer, Logo, Navigation, UserPanel } from '../../components';
+import {
+  PageContainer,
+  Logo,
+  Navigation,
+  UserPanel,
+  LogButton,
+  RegistrationButton
+} from '../../components';
+import { useAppContext } from '../../hooks';
 
 import s from './Header.module.css';
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <header className={s.header}>
       <PageContainer className={s.headerContainer}>
         <Logo className={s.logo} />
         <Navigation />
-        <UserPanel />
+        {(isLoggedIn && <UserPanel />) || (
+          <div>
+            <LogButton title="in" onClick={() => {}} />
+            <RegistrationButton />
+          </div>
+        )}
       </PageContainer>
     </header>
   );

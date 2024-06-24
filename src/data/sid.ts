d82b9d8d8245b8d4b9283b6c15869e490500a42f
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-24 23:11:13 +0200

    Create sid.ts in the /data folder
    I created the "createNanniesCollection" function. It is meant to be used to populate a database with nannies data

---
+++ src/data/sid.ts
@@ -0,0 +1,11 @@
+import { nanniesData } from '../data';
+import { dataCollectionName } from '../constants';
+import { addFile } from '../fireBase';
+
+export const createNanniesCollection = async (): Promise<void> => {
+  try {
+    await Promise.all(nanniesData.map((n) => addFile(dataCollectionName, n)));
+  } catch (error) {
+    console.error('Error creating nannies collection:', (error as Error).message);
+  }
+};

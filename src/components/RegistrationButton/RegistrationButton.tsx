c2ea19e857cfcda29fad4b2712d54b4f1b09a07b
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-24 23:48:50 +0200

    Update RegistrationButton
    - I moved Modal with RegisterForm inside this component because it seems to me like in this way it makes more sense. I also added .modal css class for it
    - Also I added border to the button for better visiblility

--- src/components/RegistrationButton/RegistrationButton.tsx
+++ src/components/RegistrationButton/RegistrationButton.tsx
@@ -1,4 +1,7 @@
 import clsx from 'clsx';
+import { MouseEvent, useState } from 'react';
+
+import { Modal, RegisterForm } from '../../components';
 
 import s from './RegistrationButton.module.css';
 
@@ -7,10 +10,26 @@ interface RegistrationButtonProps {
 }
 
 const RegistrationButton = ({ className }: RegistrationButtonProps) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleOnClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.currentTarget.blur();
+    setIsOpened(true);
+  };
+
   return (
-    <button className={clsx(className, s.button)} type="button">
-      Registration
-    </button>
+    <>
+      <button className={clsx(className, s.button)} type="button" onClick={handleOnClick}>
+        Registration
+      </button>
+      <Modal
+        className={s.modal}
+        isOpened={isOpened}
+        closeModal={() => setIsOpened(false)}
+      >
+        <RegisterForm />
+      </Modal>
+    </>
   );
 };
 

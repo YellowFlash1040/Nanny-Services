3664e55e78edf14fe0443fc8e4a154d446a11782
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-24 23:14:20 +0200

    Create fireBase confing file
    As simple as it is. Just fireBase config

---
+++ src/fireBase/fireBaseConfig.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_ID,
+  VITE_DATABASE_URL
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID,
+  databaseURL: VITE_DATABASE_URL
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const dataBase = getDatabase(app);

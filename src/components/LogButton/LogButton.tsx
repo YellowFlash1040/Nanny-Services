81da4d51b0e88f5232edc7a00e550b2f21c619dc
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-24 23:30:01 +0200

    Update for LogButton
    - I thought that it will be more logical if the LogButton will store it's modal window for loginization itself. So I changed the code accordingly to that idea.
    - I changed "title" prop from string to only two options - "in" and "out".
    - I added onClick prop for the LogOut button. I also added code to make this button usable for both scenarios - LogIn and LogOut buttons

--- src/components/LogButton/LogButton.tsx
+++ src/components/LogButton/LogButton.tsx
@@ -1,17 +1,49 @@
 import clsx from 'clsx';
+import { MouseEvent, useState } from 'react';
+
+import { Modal, LoginForm } from '../../components';
 
 import s from './LogButton.module.css';
 
 interface LogButtonProps {
   className?: string;
-  title: string;
+  title: 'in' | 'out';
+  onClick?: (event: MouseEvent) => void;
 }
 
-const LogButton = ({ title, className }: LogButtonProps) => {
+const LogButton = ({ className, title, onClick = () => {} }: LogButtonProps) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleOnClick = (event: MouseEvent<HTMLButtonElement>) => {
+    event.currentTarget.blur();
+    if (title === 'in') {
+      setIsOpened(true);
+    } else {
+      onClick(event);
+    }
+  };
+
   return (
-    <button className={clsx(s.logButton, className)} type="button">
-      Log {title}
-    </button>
+    <>
+      <button
+        className={clsx(s.logButton, className)}
+        type="button"
+        onClick={handleOnClick}
+      >
+        Log {title}
+      </button>
+      {title === 'in' && (
+        <Modal
+          className={s.modal}
+          isOpened={isOpened}
+          closeModal={() => {
+            setIsOpened(false);
+          }}
+        >
+          <LoginForm />
+        </Modal>
+      )}
+    </>
   );
 };
 
